0f9865d62eba5947a8f19a764d7cf601b070f896
Hamlet <hamletsolanod@gmail.com>
2022-06-26 11:45:46 -0600

    parentesis no reconocidos correctamente

--- src/Motomaticas/ObjetosLogicos/motorMatematico/OperacionGeneral.java
+++ src/Motomaticas/ObjetosLogicos/motorMatematico/OperacionGeneral.java
@@ -1,8 +1,6 @@
 package Motomaticas.ObjetosLogicos.motorMatematico;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
